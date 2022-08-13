f31d51b0643850f27bd7b8c33ff83f64bb746c59
Christian Martinez <crismartine.e@gmail.com>
2022-08-13 14:37:05 -0500

    Coded dictionaries

--- InterviewQuestions/list_tuples.py
+++ InterviewQuestions/list_tuples.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
 # Mutables
-list_num = [1, 2, 3, 4]
+list_num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Inmutables
 tup_num = ("Holaaa", 2, 5.6, True)
 
 print(type(list_num), list_num)
 print(type(tup_num), tup_num)
+
+# Slices start:stop:step
+print(list_num[0:6:2])
+print(list_num[2:8:2])
+print(list_num[:-1])
+print(list_num[-1:])
+print(list_num[::-1])
