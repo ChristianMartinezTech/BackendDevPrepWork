d802dd7c4390c52ed04752f7606a47bd90344a46
Christian Martinez <crismartine.e@gmail.com>
2022-08-29 21:51:17 -0500

    Coded Rectangles

---
+++ Python/0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Base of the whole program"""
+
+
+from models.base import Base
+
+class Rectangle(Base):
+    """Rectangle class"""
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    # Width Getters and Setters
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
+
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
+
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = y
