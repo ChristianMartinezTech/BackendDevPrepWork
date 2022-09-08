edd630e7a51517bb67bb631dc7a976621f42713b
Christian Martinez <christianmartinez@pop-os.localdomain>
2022-09-08 12:09:51 -0500

    Worked on Platzi, Docker and Data structures

---
+++ Platzi/PythonStructures/0x00-collections.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Collections:
+
+- Inmutable: can't be changed
+- Mutable(Dynamic): can be changed
+
+- Lineal: array, stack, queue
+- Non-linear: tree, graph
+
+- Ordered
+- Unordered
+"""
+
+def main():
+    students = ["Chris", "Jose", "Steff", "Camile", "Erick", "James", "Gabriela", "Allison"]
+    print(students)
+    print(type(students))
+
+
+if __name__ == "__main__":
+    main()
+
