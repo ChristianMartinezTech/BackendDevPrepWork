34aff5dc0e219cdba6af2e988403ba548912671b
Christian Martinez <christianmartinez@pop-os.localdomain>
2022-09-16 19:53:56 -0500

    Corrected Pep8 in the 0x00 file

--- LeetCode/0x00-two-sum.py
+++ LeetCode/0x00-two-sum.py
@@ -2,9 +2,11 @@
 """
 https://leetcode.com/problems/two-sum
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
 
 Example 1:
 Input: nums = [2,7,11,15], target = 9
@@ -13,31 +15,24 @@ Output: [0,1]
 Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
- 
-
-Constraints:
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
- 
-Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 """
 
+
 def twoSum(nums, target):
-    """ With range(len we refence the index, 
+    """ With range(len we refence the index,
     by only ranging over nums we refence the value, not the index"""
 
-    for i in range(len(nums)): 
+    for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
-            if nums[j] == target - nums[i]: # Chenged from i + j == target for running time issues
+            if nums[j] == target - \
+                    nums[i]:  # Changed from i + j == target for running time
                 return [i, j]
 
 
 def main():
-    response = twoSum([2,7,11,15], 9)
+    response = twoSum([2, 7, 11, 15], 9)
     print(response)
-    
+
 
 if __name__ == '__main__':
-  main()
+    main()
